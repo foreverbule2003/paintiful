97a658d4de750c8ea76b9c3568e8803defebf8d4
foreverbule2003 <foreverbule2003@gmail.com>
2015-11-07 16:49:50 +0800

    add & set paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "700*500", small: "350*250>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
