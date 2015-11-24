0e0b9f30d8a7f3034f930dcd04b3119092d4f951
foreverbule2003 <foreverbule2003@gmail.com>
2015-11-25 03:49:21 +0800

    test config.assets.prefix v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,6 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-  config.assets.prefix = "/current/public/assets"
+  config.assets.prefix = "/public/current/public/assets"
   #config.assets.prefix = "/current/app/assets"
 end
