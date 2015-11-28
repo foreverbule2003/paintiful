84794269eef6100e1c6660ea65a37fbfaaa850aa
foreverbule2003 <foreverbule2003@gmail.com>
2015-11-28 10:43:17 +0800

    fix index image size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
-  has_attached_file :image, styles: { medium: "1200*900", small: "400*300>"}
+  has_attached_file :image, styles: { medium: "900*900", small: "300*300>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
