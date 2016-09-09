55eba2d4f32ae894f18e8fbf91406065cf13db63
Tim <foreverbule2003@gmail.com>
2016-09-09 20:22:26 +0800

    add post_collection model test

--- spec/models/post_collection_spec.rb
+++ spec/models/post_collection_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PostCollection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'belongs_to Collection' do
+    @col = Collection.create
+    @pc = @col.post_collections.create
+    expect(@col.post_collections).to include(@pc)
+  end
+
+  it 'belongs_to Post' do
+    @p = Post.create
+    @pc1 = @p.post_collections.new
+    expect(@p.post_collections).to include(@pc1)
+  end
 end
